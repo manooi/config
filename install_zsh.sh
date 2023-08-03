7ab26cec9d4da171bbcc25220a7908076d16d6f8
sirawit.mahanin <sirawit.mn@gmail.com>
2023-08-03 23:55:19 +0700

    Add shell script for instlling zsh and vim

---
+++ install_zsh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Step 1: Update repositories
+sudo apt update
+
+sudo apt install git
+
+sudo apt install curl
+
+# Step 2: Install Zsh
+sudo apt install zsh
+
+# Step 3: Set Zsh as default shell (optional)
+chsh -s /bin/zsh
+
+# Step 4: Install Oh My Zsh (optional)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Step 5: Logout and log back in (if default shell changed)
+echo "Please log out and log back in for the changes to take effect."
+
---
+++ install_zsh_plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)/' ~/.zshrc
+
+echo "bindkey '`' autosuggest-execute" >> ~/.zshrc
